6513dbe998c0ea28ac4b07283d5eadfde73a1cc2
foxifio <foxifio00@gmail.com>
2022-06-26 19:22:10 +0300

    Creating landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burger = document.querySelector('#burger');
+
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('disp');
+}
+);
